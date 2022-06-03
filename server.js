6ec0a6206e07b52df32de586d3033b1df426001e
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-06-03 09:18:15 -0400

    edited both the inext and the notes html files as well as added a server.js file to structure and starteed to implement code inside of the server.js file

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require ("express");
+
+
+
+// todo  add the routes for the api/html
+
+const apiRoutes = require("./routes/apiRoutes");
+
+const htmlRoutes = require("./routes/htmlRoutes");
+
+
+
+//initalize the app and create a port 
+const app = express();
+
+const PORT = process.nextTick.PORT || 3001;
+
+// to do set up  a body parsing static and route and middlewear 
+app.use(express.urlencoded({ extend: true}));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/",htmlRoutes);
+
+app.use("/api",apiRoutes);
+
+
+// start the server on the port 
+
+app.listen(PORT,() => console.log(`This port is listening at ${PORT}`));
+
+
+module.exports =app;
